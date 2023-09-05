52855920da19cf85fb18f3faa3808d4f77526706
LoayAbd <83564752+LoayAbd@users.noreply.github.com>
2023-09-06 08:57:00 +0930

    Add files via upload

--- memsim.py
+++ memsim.py
@@ -86,7 +86,9 @@ def main():
     print(f"events in trace: {no_events}")
     print(f"total disk reads: {mmu.get_total_disk_reads()}")
     print(f"total disk writes: {mmu.get_total_disk_writes()}")
-    print(f"page fault rate:    {mmu.get_total_page_faults() / frames:.4f}")
+    print("page fault rate: ", end="")
+    print("{0:.4f}".format(mmu.get_total_page_faults() / no_events))
+
 
 
 if __name__ == "__main__":
